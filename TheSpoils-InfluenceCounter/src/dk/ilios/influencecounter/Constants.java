2a3becde94517026fdd12f357902df0f48196c77
Ilios <christian@ilios.dk>
2012-03-29 13:30:45 +0200

    Added new demo gfx + 1.1 release apk.

--- TheSpoils-InfluenceCounter/src/dk/ilios/influencecounter/Constants.java
+++ TheSpoils-InfluenceCounter/src/dk/ilios/influencecounter/Constants.java
@@ -2,5 +2,5 @@ package dk.ilios.influencecounter;
 
 public class Constants {
 	public static final int MAX_PLAYERS = 2; 	// Max players that can be tracked
-	public static final boolean DEBUG = true;	// Debug enabled (for improved logging)
+	public static final boolean DEBUG = false;	// Debug enabled (for improved logging)
 }
